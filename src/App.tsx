eeb1cfcefc589384fe205eaf036e3023beec1a66
ayush-wishlink <ayush.srivastava@wishlink.com>
2025-08-16 18:37:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-100 to-yellow-100">
+      <h1 className="text-4xl font-bold text-purple-700">
+        Panditji Frontend ✅ Tailwind is Working!
+      </h1>
+    </div>
+  );
+}
